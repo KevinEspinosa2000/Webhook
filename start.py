aacf7e06018af5439b391c5cab1419563a602d02
KevinEspinosa2000 <espinosasalcedokevinalexei@gmail.com>
2025-10-08 11:27:59 -0600

    Segundo commit

--- start.py
+++ start.py
@@ -8,18 +8,20 @@ import os                       # Variables de entorno
 app = Flask(__name__)
 
 # Configuracion desde variables de entorno
-ENDPOINT = os.getenv("Webhook_Endpoint", "https://ejemplo.com/webhook")
-INTERVAL = int(os.getenv("Intervalo_Webhook", 60))  #Segundos
+ENDPOINT = os.getenv("Webhook_Endpoint", "https://webhook.site/f527b9a8-210a-4bde-87d0-d5187a1923c8")
+INTERVAL = int(os.getenv("Intervalo_Webhook", 5))  #Segundos
 
 def send_message():
     while True:
         try:
-            response = requests.post(ENDPOINT, json={"message": "Webhook desde Flask"})
+            response = requests.post(ENDPOINT, json={"message": "Webhook usando Flask", "timelegible": time.strftime("%Y-%m-%d %H:%M:%S")})
             print(f"Mensaje enviado: {response.status_code}")
+            estado_previo = response.status_code
+            print(f"Status Previo: {estado_previo}")
         except Exception as e:
             print(f"Error al enviar el mensaje: {e}")
         time.sleep(INTERVAL)    
-
+# Sitio web
 @app.route("/")
 def status():
     return f"Webhook activo. Enviado a {ENDPOINT} cada {INTERVAL} segundos"
